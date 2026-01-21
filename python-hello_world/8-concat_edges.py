75b34632d031fed39348e329c96cc8e91352f4c4
Uliana Deshin <uliana.deshin@gmail.com>
2026-01-21 16:50:07 +1100

    concat edges

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+# slice and concatenate str to print "object-oriented programming language is an interpreted"
+# no loops or conditional statements
+print(str[39:66] + str[106:112] + str[:6])
