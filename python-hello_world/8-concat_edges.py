265e4069f10291e5a3aa46e561dc5a142921af11
Uliana Deshin <uliana.deshin@gmail.com>
2026-01-21 16:57:12 +1100

    add comment

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # edge slicing exercise
+# do not add variables
 str = ("Python is an interpreted, interactive, object-oriented programming " \
 "language that combines remarkable power with very clear syntax")
 print(str[39:66] + str[106:112] + str[:6])
