c1242c8b6564a2d9d54061998227c77cd79164d5
Uliana Deshin <uliana.deshin@gmail.com>
2026-01-25 16:21:22 +1100

    argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    argc = len(argv)
+    
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[0]))
+    else:
+        print("{} arguments:".format(argc))
+        for i in range(argc):
+            print("{}: {}".format(i + 1, argv[i]))
