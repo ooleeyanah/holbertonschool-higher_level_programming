0cb3538b266c58aeebe1a2c5ab29c1915567c534
Uliana Deshin <uliana.deshin@gmail.com>
2026-01-30 12:19:52 +1100

    prints int matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for index in range(len(row)):
+            if index != len(row) - 1:
+                print("{:d}".format(row[index]), end=" ")
+            else:
+                print("{:d}".format(row[index]), end="")
+        print()
