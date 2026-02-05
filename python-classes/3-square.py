a701eeb55ca4f25dd9a438c90ba4233096460fac
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-05 12:56:55 +1100

    public inst method area(self)

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that defines a Square class with size validation.
+
+"""
+
+
+class Square:
+    """
+    A class that represents a square with validated size attribute.
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square instance with size validation.
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
