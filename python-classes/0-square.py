48293140fb4874a9f04b2e7004e9bed84e8bc026
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-05 11:53:22 +1100

    empty square placeholder

---
+++ python-classes/0-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module that defines a Square class.
+This module contains the basic implementation of a Square class.
+"""
+
+
+class square:
+    """
+    A class that represents a square.
+    
+    This is a basic square class that currently serves as a placeholder
+    for future implementation of square-related functionality.
+    
+    Attributes:
+        None currently defined.
+    
+    Methods:
+        None currently defined.
+    """
+    pass
