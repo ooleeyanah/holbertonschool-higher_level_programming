4681439047a57f0c3124752eb8fc4e1246065386
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-05 12:13:30 +1100

    cut down docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 """
 Module that defines a Square class.
-This module contains the basic implementation of a Square class.
 """
 
 
 class Square:
     """
     A class that represents a square.
-    
-    This is a basic square class that currently serves as a placeholder
-    for future implementation of square-related functionality.
-    
-    Attributes:
-        None currently defined.
-    
-    Methods:
-        None currently defined.
+
     """
     pass
