43c733a4b2ffd6737d54628671d6ff645b1eb4cf
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-10 14:37:18 +1100

    is kind of class w/ isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module returns Boolean if obj is instance of a class or inherited.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if object is inst of class or inherited.
+    """
+    return isinstance(obj, a_class)
