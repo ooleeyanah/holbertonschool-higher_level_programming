8761b248359d93739b18732819324734f31082f1
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-10 11:59:09 +1100

    lookup with dir

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that looks up a list object.
+"""
+
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    """
+    return dir(obj)
