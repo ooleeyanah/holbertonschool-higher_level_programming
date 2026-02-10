5da03173a055a9af461a02da4f750474edf1c60d
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-10 12:13:31 +1100

    is same class?

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module that returns Boolean if obj is an instance of a class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if object is exactly an instance of specified class. Otherwise returns False.
+    """
+    return type(obj) is a_class
