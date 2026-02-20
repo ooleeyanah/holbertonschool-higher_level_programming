4fcb46c35df3b3ed14c32e9e05fd1239cf11a252
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-20 15:30:50 +1100

    return not print

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ Defines a function that writes a string to a text file.
 def write_file(filename="", text=""):
     """ Write a file function. """
     with open(filename, 'w', encoding="utf-8") as f:
-        print(f.write(text))
+        return f.write(text)
