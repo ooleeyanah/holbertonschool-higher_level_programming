c1d445421330a7897701050ab26e8012eb25c84e
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-20 15:37:46 +1100

    json string of obj

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines a function that returns JSON rep of obj.
+"""
+import json
+
+def to_json_string(my_obj):
+    """ Function to return JSON rep of obj. """
+    json_string = json.dumps(my_obj)
+    print(json_string)
