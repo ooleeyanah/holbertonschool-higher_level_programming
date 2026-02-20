11b9734d683c33cc242fa623c781a024168398c4
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-20 15:39:42 +1100

    blank lines and no parentheses for return

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,7 +4,8 @@ Defines a function that returns JSON rep of obj.
 """
 import json
 
+
 def to_json_string(my_obj):
     """ Function to return JSON rep of obj. """
     json_string = json.dumps(my_obj)
-    return(json_string)
+    return json_string
