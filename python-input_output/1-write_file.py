64b2cdfe04b7eb24ba150090d43aeb90fd27c16e
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-20 15:26:42 +1100

    write file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines a function that writes a string to a text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """ Write a file function. """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
