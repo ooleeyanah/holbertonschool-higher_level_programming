4199bfdd60ccc9a7f09d9de3d6b5b1321f4f3dcb
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-21 17:29:51 +1100

    save to json file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a function that writes an obj to a txt file with JSON rep.
+"""
+import json
+
+
+def save_to_json(my_obj, filename):
+    """ Function to save object to file with JSON rep. """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
