cde2e59490fe99ade3ef18498cf4e0e561a1bd67
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-21 17:30:45 +1100

    save to json FILLLLEE

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ Defines a function that writes an obj to a txt file with JSON rep.
 import json
 
 
-def save_to_json(my_obj, filename):
+def save_to_json_file(my_obj, filename):
     """ Function to save object to file with JSON rep. """
     with open(filename, 'w') as f:
         json.dump(my_obj, f)
