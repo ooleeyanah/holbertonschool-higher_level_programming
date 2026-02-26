44e27b3d2f44412159104e3782d0ea9819bfc982
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-26 11:12:31 +1100

    requests

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Module that defines functions to fetch posts from JSONPlaceHolder.
+
+"""
+import requests
+import json
+import csv
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+
+def fetch_and_print_posts():
+    """ Fetches and prints posts from JSONPlaceHolder. """
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    """ Fetches posts from JSONPlaceHolder and saves to CSV file. """
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        structured_posts = [
+            {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            }
+            for post in posts
+        ]
+
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_posts)
