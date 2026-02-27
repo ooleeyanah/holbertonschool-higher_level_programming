01c7fbcf4ff1b27293b4f44049065f3842b9fc15
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-27 14:10:19 +1100

    count

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- displays specific records in table first_table
-SELECT * FROM first_table WHERE id = 89;
+-- displays number of records with id = 89 in table first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
