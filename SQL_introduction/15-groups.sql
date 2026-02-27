adabb428d7b959496445837d8c180025137cbf76
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-27 16:01:40 +1100

    group by

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- counts num of records with same score
-SELECT score, COUNT(score) AS number FROM second_table ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
