5747be226e3bd5bbbe9210b2baff10928c5dc38c
Uliana Deshin <uliana.deshin@gmail.com>
2026-02-27 16:06:36 +1100

    name no tnull

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all columns with values
-SELECT score, name FROM second_table WHERE (score, name) IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
