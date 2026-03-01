fdbdcb759cf92c4ebf702f5dd29906ebf62b790f
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-01 15:50:37 +1100

    grant read

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- create database and user
+CREATE DATABASE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
