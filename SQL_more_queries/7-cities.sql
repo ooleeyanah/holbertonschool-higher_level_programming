b9ead840049cc47a08f0d6b3d86edfd15dfec92e
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-01 16:22:32 +1100

    foreign key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create table with foreign key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT UNIQUE NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (state_id) REFERENCES states(id)
+);
