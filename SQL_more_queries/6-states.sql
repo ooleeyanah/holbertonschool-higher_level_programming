5f781a79b17c58f24f08280d800653388ab2327c
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-01 16:24:55 +1100

    use

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- create a bit more detailed table
-USE IF EXISTS @hbtn_0d_usa;
+USE @hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa(
 id INT AUTO_INCREMENT NOT NULL,
 name VARCHAR(256) NOT NULL,
