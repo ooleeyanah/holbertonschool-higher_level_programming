308d780d919efd01bcd839fd4532540fc8f3edb6
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-01 16:24:24 +1100

    autoinc

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create a bit more detailed table
 USE IF EXISTS @hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa(
-id INT NOT NULL UNIQUE,
+id INT AUTO_INCREMENT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY (id)
 );
