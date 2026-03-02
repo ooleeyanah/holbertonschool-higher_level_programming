4456e548502b18e5523645be8d5921ebeefc9b24
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-02 15:57:27 +1100

    remove db use

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,4 @@
 -- list all shows with at least one genre linked
-USE hbtn_0d_tvshows;
 SELECT s.title, sg.genre_id FROM tv_shows AS s 
 LEFT JOIN tv_show_genres AS sg ON s.id = sg.show_id 
 ORDER BY s.title ASC, sg.genre_id ASC;
