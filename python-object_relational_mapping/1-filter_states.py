f164d8559e8a7ad5eb67f3acbbc2ceb5d2337e7f
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-06 10:12:05 +1100

    where like n%

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create cursor object
+    cursor = db.cursor()
+
+    # Execute query to select all states ordered by id
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'n%' ORDER BY id ASC")
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
