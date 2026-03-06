eaa33db2fb749624b3553aab211a3cd65a6042bd
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-06 13:59:59 +1100

    cities per state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Lists all cities from db hbtn_0e_0_usa that matches input state SAFELY
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create cursor object
+    cursor = db.cursor()
+
+    # Execute query to select all states ordered by id
+    cursor.execute("SELECT cities.name FROM cities LEFT JOIN states "
+                    "ON cities.state_id = states.id WHERE states.name = %s "
+                   "ORDER BY cities.id ASC", (state_name_searched,))
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
