8a58708fde06387dc13e3d1366ad6481ce4aa0f1
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-06 11:56:29 +1100

    .format

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa that matches input arg
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create cursor object
+    cursor = db.cursor()
+
+    # Execute query to select all states ordered by id
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name_searched))
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
