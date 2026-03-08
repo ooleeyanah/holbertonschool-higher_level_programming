c822a17da6a695b7c77d710f37cccbd46df46ac2
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-08 14:25:17 +1100

    fetch states

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Check for correct num of args
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line args
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create engine connecting to MySQL
+    engine = create_engine('mysql+pymysql://{}:{}@localhost:3306/{}'.format(
+        username, password, database), echo=False)
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all States ordered by id asc
+    states = session.query(State).order_by(State.id).all()
+
+    # Display results
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Close session
+    session.close()
