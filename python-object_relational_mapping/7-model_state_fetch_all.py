c9888dcf287fb56e7c9ba2e551bd47d4838d6c22
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-08 14:34:09 +1100

    mysqldb not pymysql

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,18 +9,13 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Check for correct num of args
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
     # Get command line args
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     # Create engine connecting to MySQL
-    engine = create_engine('mysql+pymysql://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         username, password, database), echo=False)
 
     # Create session
