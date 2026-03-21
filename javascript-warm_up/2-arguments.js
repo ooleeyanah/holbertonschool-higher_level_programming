3f2b5eb03608f9c2f9739a2fbb63e54dcee45278
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-21 20:00:31 +1100

    args

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function numOfArgs(...args) {
+    if (args.length === 0) {
+        console.log('No argument');
+    } else if (args.length === 1) {
+        console.log('Argument found');
+    } else {
+        console.log('Arguments found');
+    };
+}
