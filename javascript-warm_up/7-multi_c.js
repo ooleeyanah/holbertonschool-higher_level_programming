64cb46b7f0859e17a89861d23eec2d5e50e95b42
Uliana Deshin <uliana.deshin@gmail.com>
2026-03-21 20:22:24 +1100

    print x num of times

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const x = process.argv[2];
+const number = parseInt(x);
+
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
+}
